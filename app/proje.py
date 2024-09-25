7acd05120cb329a9d7358694a758eb7f0e1dc6ab
arifshn <habibarifsahin@gmail.com>
2024-09-25 21:51:54 +0300

    Compose Project

---
+++ app/proje.py
@@ -0,0 +1,17 @@
+#Bazı temel bileşenleri framework den içe aktarıyorum 
+from flask import Flask, render_template, request
+#python ile yazılmış web uygulamalarında  web çatısı olarak çalışır
+app = Flask(__name__)
+#fonksiyonları tanımlıyorum 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    return render_template('success.html', name=name, email=email)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
